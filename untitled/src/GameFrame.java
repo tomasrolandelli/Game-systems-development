bd8190b0cc6c6d383f0685823eba31455642ad8b
tomasrolandelli <80985920+tomasrolandelli@users.noreply.github.com>
2024-05-15 13:19:48 +0200

    Game frame class

--- untitled/src/GameFrame.java
+++ untitled/src/GameFrame.java
@@ -1,2 +1,18 @@
-public class GameFrame {
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+    GamePanel panel = new GamePanel();
+
+    GameFrame() {
+        this.add(this.panel);
+        this.setTitle("2D Ping-Pong");
+        this.setResizable(false);
+        this.setBackground(new Color(31, 78, 47));
+        this.setDefaultCloseOperation(3);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo((Component)null);
+    }
 }
