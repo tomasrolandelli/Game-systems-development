004ea2324df1c433f997a28d9138c0b59ed0e20f
tomasrolandelli <80985920+tomasrolandelli@users.noreply.github.com>
2024-05-15 22:49:42 +0200

    menu frame class completed

--- untitled/src/MenuFrame.java
+++ untitled/src/MenuFrame.java
@@ -1,2 +1,20 @@
-public class MenuFrame {
+import javax.swing.*;
+
+
+public class MenuFrame extends JFrame {
+
+    MenuPanel panel;
+
+    MenuFrame() {
+        panel = new MenuPanel();
+        this.add(panel);
+        this.setTitle("Main Manu for Ping-Pong");
+        this.setResizable(false);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
 }
