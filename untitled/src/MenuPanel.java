09d7f54bca54c9e2ed3f63c3752df8d5ce12eab3
tomasrolandelli <80985920+tomasrolandelli@users.noreply.github.com>
2024-05-15 22:50:16 +0200

    menu panel complete

--- untitled/src/MenuPanel.java
+++ untitled/src/MenuPanel.java
@@ -1,2 +1,41 @@
-public class MenuPanel {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JPanel;
+
+public class MenuPanel extends JPanel {
+    MenuPanel() {
+        this.addKeyListener(new AL(this));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.setPreferredSize(GamePanel.SCREEN);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Consolas", 1, 70));
+        g.drawString("GAME MENU", 330, 100);
+        g.setFont(new Font("Consolas", 1, 30));
+        g.drawString("Press 'ENTER' to start the game", 265, 200);
+        g.setFont(new Font("Consolas", 0, 10));
+    }
+
+    public class AL extends KeyAdapter {
+        public AL(final MenuPanel this$0) {
+        }
+
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == 10) {
+                new GameFrame();
+            }
+
+        }
+    }
 }
